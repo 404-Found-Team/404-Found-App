69c99da94de43dc865c19048e5c52a7d91763ece
Trey Palmer <treypalmer33@gmail.com>
2026-02-09 17:04:08 -0500

    Added OAuth endpoints, but OAuth consent screen and Google Cloud Project must be configured for functionality

--- Backend/APIService/main.py
+++ Backend/APIService/main.py
@@ -4,7 +4,29 @@ import uvicorn
 from api.api_v1 import api_router
 
 # Create a FastAPI app
-app = FastAPI(title="404 Found API")
+
+
+from authlib.integrations.starlette_client import OAuth
+from starlette.config import Config
+import os
+from contextlib import asynccontextmanager
+
+
+@asynccontextmanager
+async def lifespan(app):
+    config = Config(environ=os.environ)
+    oauth = OAuth(config)
+    oauth.register(
+        name='google',
+        client_id=os.environ.get('GOOGLE_CLIENT_ID', ''),
+        client_secret=os.environ.get('GOOGLE_CLIENT_SECRET', ''),
+        server_metadata_url='https://accounts.google.com/.well-known/openid-configuration',
+        client_kwargs={'scope': 'openid email profile'}
+    )
+    app.state.oauth = oauth
+    yield
+
+app = FastAPI(title="404 Found API", lifespan=lifespan)
 
 app.include_router(api_router)
 
