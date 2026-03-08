d6fcba1817fb8aebb83a3a558b0c4812ad115149
Trey Palmer <treypalmer33@gmail.com>
2026-03-08 01:00:59 -0500

    Added Makefile for easier setup and running of the backend

--- Backend/APIService/api/api_v1.py
+++ Backend/APIService/api/api_v1.py
@@ -5,9 +5,10 @@ API v1 router registration, including OAuth2 endpoints.
 
 from fastapi import APIRouter
 
-from api.routers import auth_routers, oauth_routers, data_ingestion_routers
+from api.routers import auth_routers, oauth_routers, data_ingestion_routers, safety_alert_routers
 
 api_router = APIRouter(prefix="/api/v1")
 api_router.include_router(auth_routers.router)
 api_router.include_router(oauth_routers.router)
 api_router.include_router(data_ingestion_routers.router)
+api_router.include_router(safety_alert_routers.router)
--- Backend/APIService/api/routers/safety_alert_routers.py
+++ Backend/APIService/api/routers/safety_alert_routers.py
@@ -1,9 +1,22 @@
 from fastapi import APIRouter, Depends, HTTPException
 from api.deps import db_dependency
 from schemas.safety_feed_schema import SafetyCreate, SafetyDataResponse
+from crud import safety as s
 
 router = APIRouter(prefix="/safety", tags=["alerts"])
 
 @router.post("/")
 def post_alert(alert: SafetyCreate, db: db_dependency):
-    pass
+    return s.insert_alert(db, alert)
+
+@router.get("/", response_model=SafetyDataResponse)
+def fetch_alerts(db: db_dependency):
+    return s.get_alerts(db)
+
+@router.post("/upvote")
+def upvote_alert(alert_id: int, db: db_dependency):
+    return s.upvote_alert(db, alert_id)
+
+@router.post("/downvote")
+def downvote_alert(alert_id: int, db: db_dependency):
+    return s.downvote_alert(db, alert_id)
